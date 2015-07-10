551dc79acc8c129130e6c9a35a18931a936a442a
Tom Copeland <tom.copeland@livingsocial.com>
2015-07-10 08:03:52 -0400

    Remove enemies when they go offscreen

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -3,8 +3,16 @@
 GameState::GameState(Spritesheet* spritesheet) {}
 
 void GameState::update() {
+  std::vector<GameObject*> to_remove;
   for (std::vector<GameObject*>::iterator i = m_gameObjects.begin(); i != m_gameObjects.end(); i++) {
-    (*i)->update();
+    GameObject* obj = *i;
+    obj->update();
+    if (obj->getPosition().getY() > 600) {
+      to_remove.push_back(obj);
+    }
+  }
+  for (std::vector<GameObject*>::iterator i = to_remove.begin(); i != to_remove.end(); i++) {
+    m_gameObjects.erase(std::remove(m_gameObjects.begin(), m_gameObjects.end(), *i));
   }
 }
 
